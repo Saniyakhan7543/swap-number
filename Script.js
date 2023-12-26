095fb505e2fd47f61696d951e41729536e60c354
Saniya khan <133801644+Saniyakhan7543@users.noreply.github.com>
2023-12-26 17:58:22 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,9 @@
+
+
+    function swap(){
+        var a,b;
+        a=Number(document.getElementById("first").value);
+        b=Number(document.getElementById("second").value);
+        document.getElementById("After_Swap_First_Value").value= b;
+        document.getElementById("After_Swap_Second_Value").value= a;
+        }
